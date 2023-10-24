24916e09167ec1839a2d1f1ebf1f80afde4028fa
Denis Stael <denis@nullbug.dev>
2023-10-24 02:40:39 -0300

    Option to redis password in gush configuration

--- lib/gush/client.rb
+++ lib/gush/client.rb
@@ -11,7 +11,7 @@ module Gush
       cached = (@@redis_connection.value ||= { url: config.redis_url, connection: nil })
       return cached[:connection] if !cached[:connection].nil? && config.redis_url == cached[:url]
 
-      Redis.new(url: config.redis_url).tap do |instance|
+      Redis.new(url: config.redis_url, password: config.redis_password).tap do |instance|
         RedisClassy.redis = instance
         @@redis_connection.value = { url: config.redis_url, connection: instance }
       end
--- spec/gush/configuration_spec.rb
+++ spec/gush/configuration_spec.rb
@@ -5,6 +5,7 @@ describe Gush::Configuration do
   it "has defaults set" do
     subject.gushfile = GUSHFILE
     expect(subject.redis_url).to eq("redis://localhost:6379")
+    expect(subject.redis_password).to be_nil
     expect(subject.concurrency).to eq(5)
     expect(subject.namespace).to eq('gush')
     expect(subject.gushfile).to eq(GUSHFILE.realpath)
@@ -16,12 +17,14 @@ describe Gush::Configuration do
     it "allows setting options through a block" do
       Gush.configure do |config|
         config.redis_url = "redis://localhost"
+        config.redis_password = "redis_password"
         config.concurrency = 25
         config.locking_duration = 5
         config.polling_interval = 0.5
       end
 
       expect(Gush.configuration.redis_url).to eq("redis://localhost")
+      expect(Gush.configuration.redis_password).to eq("redis_password")
       expect(Gush.configuration.concurrency).to eq(25)
       expect(Gush.configuration.locking_duration).to eq(5)
       expect(Gush.configuration.polling_interval).to eq(0.5)
